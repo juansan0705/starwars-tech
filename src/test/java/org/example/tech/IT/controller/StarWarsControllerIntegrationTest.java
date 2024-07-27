67b9c90b433cd7eb8287780840eed29696627417
Juan Sánchez Crespillo <101835346+juansan0705@users.noreply.github.com>
2024-07-27 19:03:03 +0200

    fix tests

--- src/test/java/org/example/tech/IT/controller/StarWarsControllerIntegrationTest.java
+++ src/test/java/org/example/tech/IT/controller/StarWarsControllerIntegrationTest.java
@@ -2,53 +2,60 @@ package org.example.tech.IT.controller;
 
 import org.example.tech.model.People;
 import org.example.tech.model.Starship;
+import org.example.tech.service.SWAPIService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
+import java.util.Collections;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class StarWarsControllerIntegrationTest {
-
-    @LocalServerPort
-    private int port;
+@SpringBootTest
+@AutoConfigureMockMvc
+class StarWarsControllerIntegrationTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SWAPIService swapiService;
 
     @Test
-    public void getPeople() {
-        ResponseEntity<List<People>> response = restTemplate.exchange(
-                "http://localhost:" + port + "/api/v1/star-wars/people",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<People>>() {}
-        );
-        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-        assertThat(response.getBody()).isNotEmpty();
+    void testGetPeople() throws Exception {
+        when(swapiService.getAllPeople("name", true)).thenReturn(Collections.singletonList(new People("Luke Skywalker", "19BBY", "male", "2023-10-01T00:00:00Z")));
 
-        assertThat(response.getBody().get(0).getName()).isNotNull();
+        mockMvc.perform(get("/api/v1/star-wars/people?sortBy=name&ascending=true"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Luke Skywalker"));
     }
 
     @Test
-    public void getStarships() {
-        ResponseEntity<List<Starship>> response = restTemplate.exchange(
-                "http://localhost:" + port + "/api/v1/star-wars/starships",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<Starship>>() {}
-        );
-        assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-        assertThat(response.getBody()).isNotEmpty();
+    void testGetStarships() throws Exception {
+        when(swapiService.getAllStarships("name", true)).thenReturn(Collections.singletonList(new Starship("X-wing", "T-65 X-wing", "Incom Corporation", "2023-10-01T00:00:00Z")));
 
-        assertThat(response.getBody().get(0).getName()).isNotNull();
+        mockMvc.perform(get("/api/v1/star-wars/starships?sortBy=name&ascending=true"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("X-wing"));
+    }
+    @Test
+    void testGetPeopleFailure() throws Exception {
+        when(swapiService.getAllPeople("name", true)).thenReturn(Collections.singletonList(new People("Luke Skywalker", "19BBY", "male", "2023-10-01T00:00:00Z")));
+
+        mockMvc.perform(get("/notvalidurl"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetStarshipsFailure() throws Exception {
+        when(swapiService.getAllStarships("name", true)).thenReturn(Collections.singletonList(new Starship("X-wing", "T-65 X-wing", "Incom Corporation", "2023-10-01T00:00:00Z")));
+
+        mockMvc.perform(get("/notvalidurl"))
+                .andExpect(status().isNotFound());
     }
 }
